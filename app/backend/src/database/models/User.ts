ede86d3183b5a624d9d0389aa8c05bc6c99f4787
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-07-06 19:17:47 -0300

    fix: Models - User

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,47 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class User extends Model {
+  public id: number;
+  public username: string;
+  public role: string;
+  public email: string;
+  public password: string;
+}
+
+User.init({
+  id: {
+    allowNull: false,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  username: {
+    type: DataTypes.STRING },
+  email: {
+    type: DataTypes.STRING,
+  },
+  password: {
+    type: DataTypes.STRING,
+  },
+  image: {
+    type: DataTypes.STRING },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  modelName: 'User',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default User;
