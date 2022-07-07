65bebddedacb0dbda111264875e6d7ba8e3eb070
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-07-07 08:51:50 -0300

    fix: UsersModel inclusao do atributo autoincrement

--- app/backend/src/database/models/UsersModel.ts
+++ app/backend/src/database/models/UsersModel.ts
@@ -3,7 +3,7 @@ import db from '.';
 // import OtherModel from './OtherModel';
 
 class User extends Model {
-  public id: number;
+  public id!: number;
   public username: string;
   public role: string;
   public email: string;
@@ -13,25 +13,30 @@ class User extends Model {
 User.init({
   id: {
     allowNull: false,
+    autoIncrement: true,
     primaryKey: true,
     type: DataTypes.INTEGER,
   },
   username: {
+    allowNull: false,
     type: DataTypes.STRING },
   email: {
+    allowNull: false,
     type: DataTypes.STRING,
   },
   password: {
+    allowNull: false,
     type: DataTypes.STRING,
   },
   image: {
+    allowNull: false,
     type: DataTypes.STRING },
 }, {
-  // ... Outras configs
-  underscored: true,
+  modelName: 'Users',
   sequelize: db,
-  modelName: 'User',
+  tableName: 'users',
   timestamps: false,
+  underscored: true,
 });
 
 /**
