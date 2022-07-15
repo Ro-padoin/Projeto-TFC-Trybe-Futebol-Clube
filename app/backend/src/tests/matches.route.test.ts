123b64a5e9dc004dab3b9d361b1362ac425d15ec
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-07-15 13:09:13 -0300

    feat: ultimos testes para finalizacao do projeto

--- app/backend/src/tests/matches.route.test.ts
+++ app/backend/src/tests/matches.route.test.ts
@@ -16,6 +16,7 @@ const { expect } = chai;
 let chaiHttpResponse: Response;
 
 const MATCHES_NOT_FOUND = 'Matches not found.';
+const MATCH_NOT_FOUND = 'Match not found.';
 const INVALID_TOKEN = 'Token must be a valid token';
 
 describe('Teste a rota get /matches com dados no banco de dados', () => {
@@ -129,4 +130,64 @@ const MATCHES_MOCK = {
  expect(chaiHttpResponse.body.message).to.be.equal(INVALID_TOKEN);      
   });
 
+});
+
+describe('Teste a rota patch /matches/id/finish para setar com sucesso uma partida como inProgress para false ', () => {
+
+const MOCK_FINISHED = { message: 'Finished' } as any;
+  
+    before(() => {
+      return sinon
+        .stub(MatchesModel, 'update')
+        .resolves(MOCK_FINISHED);
+    });
+  
+    after(()=>{
+      (MatchesModel.update as sinon.SinonStub).restore();
+    })
+  
+    it('Teste se com token válido, a rota de atualizacao retorna status 200 e a mensagem Finished', async () => {
+  
+      const token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AYWRtaW4uY29tIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU3NjY1NTU5LCJleHAiOjE5NzMwMjU1NTl9.8NouXtkTlj-_akkrMm9DBpfXy5dne7WYB7XBrIZeGNw"
+  
+        chaiHttpResponse = await chai
+           .request(app).patch('/matches/48/finish').set('Authorization', token).send({ inProgress: false});      
+        expect(chaiHttpResponse.status).to.be.equal(StatusCodes.OK);
+        expect(chaiHttpResponse.body).to.be.eql(MOCK_FINISHED);      
+    }); 
+  });
+
+describe('Teste a rota patch /matches/id/finish com id nao encontrado no Banco de Dados', () => {
+  const MOCK_MATCH_BY_ID = {
+    id: 48,
+    homeTeam: 13,
+    homeTeamGoals: 1,
+    awayTeam: 2,
+    awayTeamGoals: 1,
+    inProgress: false
+  } as any;
+  
+  before(() => { 
+    return sinon.stub(MatchesModel, 'findByPk').resolves(MOCK_MATCH_BY_ID);
+  });
+  
+  after(()=>{
+    (MatchesModel.findByPk as sinon.SinonStub).restore();
+  })
+  
+  it('Teste se com token válido e id nao encontrado, a rota de atualizacao retorna status 404 e a mensagem Match not found.', async () => {
+
+  const token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AYWRtaW4uY29tIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU3NjY1NTU5LCJleHAiOjE5NzMwMjU1NTl9.8NouXtkTlj-_akkrMm9DBpfXy5dne7WYB7XBrIZeGNw"
+  try {
+    chaiHttpResponse = await chai.request(app).patch('/matches/1000/finish')
+  .set('Authorization', token).send({ inProgress: false});
+  } catch (error) {
+    expect(chaiHttpResponse.status).to.be.equal(StatusCodes.NOT_FOUND);
+    expect(chaiHttpResponse.body.message).to.be.equal(MATCH_NOT_FOUND);
+  }
+  // if (!MOCK_MATCH_BY_ID) {
+  //   expect(chaiHttpResponse.status).to.be.equal(StatusCodes.NOT_FOUND);
+  //   expect(chaiHttpResponse.body.message).to.be.equal(MATCH_NOT_FOUND);
+  // } outra forma de testar
+  }); 
 });
